62611f5ca86baaa0925f0c05ffbfdeba2bf86f72
Christoffer Fink <chrfin-4@student.ltu.se>
2021-09-21 08:58:55 +0200

    Added Trace
    Also changed os.WriteFile to ioutil.WriteFile for
    backward compatibility.

--- src/d7024e/utils.go
+++ src/d7024e/utils.go
@@ -1,6 +1,11 @@
 package d7024e
 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+)
 
 var filepath string = "/app/filestorage/"
 
@@ -13,10 +18,12 @@ func RemoveContact(shortlist []Contact, contact Contact) []Contact {
 	}
 	return result
 }
+
 func WriteToFile(data []byte, filename string) {
-	err := os.WriteFile(filepath+filename, data, 0644)
+	err := ioutil.WriteFile(filepath+filename, data, 0644)
 	handleErr(err)
 }
+
 func Find(slice []Contact, val Contact) (int, bool) {
 	for i, item := range slice {
 		if item.ID.Equals(val.ID) {
@@ -24,4 +31,18 @@ func Find(slice []Contact, val Contact) (int, bool) {
 		}
 	}
 	return -1, false
+}
+
+func Trace(fname string, args ...interface{}) {
+	defer trace(fname, args...)()
+}
+
+func trace(fname string, args ...interface{}) func() {
+	start := time.Now()
+	s := fmt.Sprint(args...)
+	log.Printf("Entering %s(%s)", fname, s)
+	return func() {
+		end := time.Since(start)
+		log.Printf("Exited %s(%s) after (%.3fs)\n", fname, s, end.Seconds())
+	}
 }
